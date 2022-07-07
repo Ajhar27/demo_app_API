fe4d5411b715c9c5bc098f1d7ede7f8cf9b84929
Ajhar27 <ajharkhan48@gmail.com>
2022-07-07 14:18:48 +0530

    jj

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -17,6 +17,7 @@ namespace demo_app_API
     {
         public int Id { get; set; }
         public DateTime? C_date { get; set; }
+
         public string Name { get; set; }
         public string Mobile { get; set; }
         public string Payment { get; set; }
